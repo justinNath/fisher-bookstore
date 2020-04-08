e23df17ab3398dd0cb7beb10c54cfb3261b65303
Justin Nath <nath.36@osu.edu>
2020-04-08 11:57:19 -0400

    commit

--- src/authors/Authors.js
+++ src/authors/Authors.js
@@ -1,57 +1,15 @@
-import React, {Component} from "react";
-import "./Authors.css";
+import React, {useEffect, useState} from "react";
 import { AuthorsDisplay } from "./AuthorsDisplay";
 
-export default class Authors extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            authors: [
-            {
-                id: 1,
-                first: "Eric",
-                last: "Evans",
-            },
-            {
-                id: 2,
-                first: "Nicole",
-                last: "Forsgren",
-            },
-            {
-                id: 3,
-                first: "Will",
-                last: "Scheffter",
-            },
-            {
-                id: 4,
-                first: "Justin",
-                last: "Nath",
-            },
-            {
-                id: 5,
-                first: "Cleveland",
-                last: "Browns",
-            },
-            {
-                id: 6,
-                first: "Pitt",
-                last: "Steelers",
-            },
-            {
-                id: 7,
-                first: "Few",
-                last: "People",
-            },
-            {
-                id: 8,
-                first: "Mike",
-                last: "Tomlin",
-            }
-         ]
-      };
-    }
+export default function Authors(props) {
+    const [data, setData] = useState([]);
 
-    render() {
+    useEffect(() => {
+        fetch("https://localhost:5001/api/authors/")
+        .then(response => response.json())
+        .then(data => setData(data));
+    }, []);
+
         return (
             <div className="Books">
                 <div className="lander">
@@ -60,4 +18,3 @@ export default class Authors extends Component {
             </div>
         );
     }
-}
--- src/books/Books.js
+++ src/books/Books.js
@@ -1,65 +1,15 @@
-import React, {Component} from "react";
-import "./Books.css";
+import React, {useEffect, useState} from "react";
 import { BookDisplay } from "./BookDisplay";
 
-export default class Books extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            books: [
-            {
-                id: 1,
-                title: "Domain Driven Design",
-                author: "Eric Evans",
-                isbn: "978-0321125217",
-            },
-            {
-                id: 2,
-                title: "Accelerate",
-                author: "Nicole Forsgren",
-                isbn: "978-1942788331",
-            },
-            {
-                id: 3,
-                title: "Slow Down",
-                author: "Will Scheffter",
-                isbn: "978-1942788332",
-            },
-            {
-                id: 4,
-                title: "Stop",
-                author: "Justin Nath",
-                isbn: "978-1942788333",
-            },
-            {
-                id: 5,
-                title: "Backwards",
-                author: "Cleveland Browns",
-                isbn: "978-1942788334",
-            },
-            {
-                id: 6,
-                title: "Forwards",
-                author: "Pitt Steelers",
-                isbn: "978-1942788335",
-            },
-            {
-                id: 7,
-                title: "I love coding",
-                author: "Few People",
-                isbn: "978-1942788336",
-            },
-            {
-                id: 8,
-                title: "Go Steelers",
-                author: "Mike Tomlin",
-                isbn: "978-1942788337",
-            }
-         ]
-      };
-    }
+export default function Books(props) {
+    const [data, setData] = useState([]);
 
-    render() {
+    useEffect(() => {
+        fetch("https://localhost:5001/api/books/")
+        .then(response => response.json())
+        .then(data => setData(data));
+    }, []);
+
         return (
             <div className="Books">
                 <div className="lander">
@@ -68,4 +18,3 @@ export default class Books extends Component {
             </div>
         );
     }
-}
